95d8a209edea6e341f17e6fb8a3bab7e64cd166e
Urlovewhawa <whawa@Whawa-3.local>
2024-10-23 23:50:50 +0700

    first commit

---
+++ Assignment/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		//MainPage = new Assignment8();
+		MainPage = new Assignment9();
+	}
+}
---
+++ Assignment/Assignment10.xaml.cs
@@ -0,0 +1,9 @@
+namespace Assignment;
+
+public partial class Assignment10 : ContentPage
+{
+	public Assignment10()
+	{
+		InitializeComponent();
+	}
+}
